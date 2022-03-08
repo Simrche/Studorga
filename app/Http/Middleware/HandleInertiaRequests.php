7e955cc5f8871b5c2788bb9788f5844a43b85169
Simon Roche <simon.roche@du.devinci.fr>
2022-03-08 14:18:50 +0100

    We only see our internships now

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -41,9 +41,7 @@ class HandleInertiaRequests extends Middleware
             'appName' => config('app.name'),
 
             // Lazily
-            'auth.user' => fn () => $request->user()
-                ? $request->user()->only('id', 'name', 'email')
-                : null,
+            'auth.user' => fn () => $request->user() ? $request->user()->only('id', 'name', 'email') : null,
         ]);
 
 
